01852644da2d2852cb6eddaa24ffa96755832a13
q505507538 <505507538@qq.com>
2015-09-02 22:18:16 +0800

    增加数据返回接收方法

--- app/src/main/java/com/jay/android/fragmentforhost/MainActivity.java
+++ app/src/main/java/com/jay/android/fragmentforhost/MainActivity.java
@@ -3,23 +3,34 @@ package com.jay.android.fragmentforhost;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.jay.android.fragmentforhost.Help.BLEHelp;
 import com.jay.android.fragmentforhost.Help.CRCHelp;
+import com.jay.android.fragmentforhost.Help.DataHelp;
+import com.jay.android.fragmentforhost.Utils.HexUtils;
 import com.jay.android.fragmentforhost.Utils.UIUtils;
+import com.mt.ble.mtble.MTBLEMBLE;
 
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Fullscreen;
 import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.WindowFeature;
 
+@Fullscreen
 @EActivity(R.layout.activity_main)
+@WindowFeature({ Window.FEATURE_NO_TITLE, Window.FEATURE_INDETERMINATE_PROGRESS })
 public class MainActivity extends BaseActivity {
     private byte[] sendbytes = null;
     Intent intent;
@@ -58,20 +69,9 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE); // 隐藏标题
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);// 设置全屏
-        bleHelp = new BLEHelp(MainActivity.this);
-    }
-
-    @Override
-    protected void onResume() {
-        /**
-         * 设置为横屏
-         */
-        if (getRequestedOrientation() != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        }
-        super.onResume();
+        bleHelp = new BLEHelp(MainActivity.this, blecallback);
+        if(!DataHelp.isConnected) Toast.makeText(this, "连接成功", Toast.LENGTH_SHORT).show();
+        DataHelp.isConnected = true;
     }
 
     @Override
@@ -141,5 +141,50 @@ public class MainActivity extends BaseActivity {
         sendbytes = CRCHelp.CRC16("b3030a010000003b11220d0a");
         bleHelp.sendDatas(sendbytes);
         UIUtils.showToastSafe("急停b3030a010000003b11220d0a");
+    }
+
+    // 设置回调方法
+    private MTBLEMBLE.CallBack blecallback = new MTBLEMBLE.CallBack() {
+
+        @Override
+        public void onReviceDatas(final BluetoothGattCharacteristic data_char) {
+//            disDatas(data_char);
+        }
+
+        @Override
+        public void onReviceCMD(BluetoothGattCharacteristic data_char) {}
+
+        @Override
+        public void onDisconnect() {
+
+        }
+    };
+
+    // 显示接收数据和命令
+    private Handler handl = new Handler();
+    private boolean disDatas(final BluetoothGattCharacteristic data_char) {
+        handl.post(new Runnable() {
+            @Override
+            public void run() {
+                switch (1) {
+                    case 0: // String
+                        UIUtils.showToastSafe(data_char.getStringValue(0));
+                        break;
+                    case 1: // 16进制
+                        UIUtils.showToastSafe(HexUtils.Bytes2HexString(data_char.getValue()));
+                        break;
+                    case 2: // 10进制
+                        int count = 0;
+                        byte[] tmp_byte = data_char.getValue();
+                        for (int i = 0; i < tmp_byte.length; i++) {
+                            count *= 256;
+                            count += (tmp_byte[tmp_byte.length - 1 - i] & 0xFF);
+                        }
+                        UIUtils.showToastSafe("" + count);
+                        break;
+                }
+            }
+        });
+        return true;
     }
 }
